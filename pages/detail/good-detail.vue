cde5a1eb32de70e8cfeda7b6c50082b0d278de04
zq13408401721 <zq13408401721@139.com>
2020-10-06 10:30:08 +0800

    新品列表+商品购买详情

---
+++ pages/detail/good-detail.vue
@@ -0,0 +1,171 @@
+<template>
+	<view>
+		<view>
+			<!--轮播图-->
+			<view></view>
+			<!--信息-->
+			<view></view>
+			<!--评论-->
+			<view></view>
+			<!--参数-->
+			<view></view>
+			<!--详情-->
+			<view v-html="this.goodInfo">
+				
+			</view>
+		</view>
+		
+		<view class="dialog-box">
+			<!--弹框-->
+			<view class="dialog">
+				<!--弹框内容区域-->
+				<view class="dialog-area"></view>
+			</view>
+			<!--底部导航-->
+			<view class="bottom">
+				<view class="item">
+					<image src="../../static/ic_collect.png"></image>
+				</view>
+				<view class="cart">
+					<image src="../../static/img_cart.png"></image>
+					<text class="number">1</text>
+				</view>
+				<view class="item">
+					<text class="buy">立即购买</text>
+				</view>
+				<view class="item">
+					<text class="add-cart">加入购物车</text>
+				</view>
+			</view>
+		</view>
+	</view>
+	
+</template>
+
+<script>
+	export default{
+		data(){
+			return {
+				goodInfo:"",
+			}
+		},
+		onLoad(options) {
+			this.getGoodInfo(1155000);
+		},
+		methods:{
+			getGoodInfo:function(id){
+				var url="https://cdplay.cn/api/goods/detail?id="+id;
+				this.$http.get(url).then((response)=>{
+					console.log("response:"+response);
+					this.goodInfo = response.body.data.info.goods_desc;
+				},(error)=>{
+					console.log("error:"+error);
+				});
+			}
+		}
+	}
+	
+	
+</script>
+
+<style>
+	
+	html,body{
+		padding: 0;
+		margin: 0;
+	}
+	
+	.dialog-box{
+		width: 100%;
+		height: 100%;
+		display: flex;
+		display: -webkit-flex;
+		align-items: flex-end;
+	}
+	
+	.dialog-box .dialog{
+		width: 100%;
+		height: 100%;
+		background-color: #50555555;
+		position: fixed;
+		bottom: 100rpx;
+		display: none;
+	}
+	
+	.dialog-box .dialog .dialog-area{
+		height: 500rpx;
+		width: 100%;
+		background-color: white;
+		position: fixed;
+		bottom: 100rpx;
+	}
+	
+	/* 底部对齐 */
+	.dialog-box .bottom{
+		height: 100rpx;
+		width: 100%;
+		position: fixed;
+		bottom: 0;
+		border: 1rpx solid #F1F1F1;
+		display: flex;
+		display: -webkit-flex;
+		flex-direction: row;
+		justify-content: space-around;
+	}
+	.dialog-box .bottom .item{
+		width: 100%;
+		height: 100rpx;
+		line-height: 100rpx;
+		border: 1rpx solid #F1F1F1;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.dialog-box .bottom .item .number{
+		width: 30rpx;
+		height: 30rpx;
+		border-radius: 30rpx;
+		color: block;
+		font-size: 10rpx;
+		position: absolute;
+	}
+	.dialog-box .bottom .cart{
+		width: 100%;
+		height: 100rpx;
+		border: 1rpx solid #F1F1F1;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.dialog-box .bottom .cart image{
+		position: absolute;
+	}
+	.dialog-box .bottom .cart text{
+		position: absolute;
+		width: 30rpx;
+		height: 30rpx;
+		border-radius: 30rpx;
+		color: block;
+		font-size: 10rpx;
+		background-color: #A52A2A;
+		margin-top: -20rpx;
+		margin-left: 20rpx;
+		color: white;
+	}
+	.dialog-box .bottom image{
+		width: 50rpx;
+		height: 50rpx;
+	}
+	.dialog-box .bottom text{
+		text-align: center;
+		font-size: 28rpx;
+	}
+	.dialog-box .bottom .buy{
+		
+	}
+	.dialog-box .bottom .add-cart{
+		width: 100%;
+		background-color: #A52A2A;
+		color: white;
+	}
+</style>
--- readme.txt
+++ readme.txt
@@ -16,3 +16,76 @@ news项目框架介绍
 1.在项目的根目录下有个pages.json 存放的是所有的页面文件路径
 
 2.pages中的page数据存放文件路径，tabBar这个数组存放底部导航的按钮
+
+
+
+/**************************************混合开发/小程序************************************/
+混合开发：跨平台
+
+
+小程序
+
+
+uni-app：开发模板
+
+
+xbuilder：ide 编写代码的工具
+
+
+VUE/react: web前端框架
+
+/***************************************学习的步骤***************************************/
+
+界面
+{
+	css样式：<style></style>
+	布局:<template><view></view></template>
+	脚本:<scripte></scripte>
+}
+
+数据加载
+{
+	uni-app
+	axios
+	jquery
+	ajax
+	resource
+}
+
+1.style
+	文档流
+	
+	布局：flex
+	flex显示方向：flex-direction:row/column
+	justify-content: 子布局显示的方式，权重平分/不平分/从左到右
+	相对/绝对 :relative/absolute
+	absolute：脱离文档流
+	
+	浮动：float——脱离文档流，在它的上一个非float标签的下边
+	网格：grid
+	grid-template-rows/grid-template-columns ：行高/列数
+	grid-rows-gap/grid/columns-gap:行间距/列间距
+	
+	display：flex/relative/absolute/fiexd/
+
+
+2.view
+	v-for v-if v-show
+	:class :src
+
+
+3.scripte 脚本
+	生命周期的钩子函数https://blog.csdn.net/qq_35585701/article/details/81216704
+	
+	data(){} 定义变量
+	props(){}   https://cn.vuejs.org/v2/guide/components-props.html
+	watch(){}和computed区别 https://www.cnblogs.com/jiajialove/p/11327945.html
+	methods(){} 定义自定义函数
+	
+
+
+
+
+
+
+
